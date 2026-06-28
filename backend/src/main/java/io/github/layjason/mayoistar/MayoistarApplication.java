9383d0b5b3e715e6b46dfc2e7a251e50e3c581a5
NonsenseCourse <nonsensecourse@163.com>
2026-06-28 15:15:26 +0800

    init: 初始化后端、Web端项目

---
+++ backend/src/main/java/io/github/layjason/mayoistar/MayoistarApplication.java
@@ -0,0 +1,12 @@
+package io.github.layjason.mayoistar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayoistarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MayoistarApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/io/github/layjason/mayoistar/MayoistarApplicationTests.java
@@ -0,0 +1,11 @@
+package io.github.layjason.mayoistar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayoistarApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
