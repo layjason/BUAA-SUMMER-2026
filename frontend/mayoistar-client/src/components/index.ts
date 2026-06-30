bdafcf7be285600c010d351bfac619581ed7abef
Lance_zer <Lance_zer@outlook.com>
2026-06-30 22:38:02 +0800

    feat(frontend): 新增 components 统一导出
    集中导出 PageHeader、FormInput、FormError、SubmitButton、CooldownButton 及 useCooldown。

---
+++ frontend/mayoistar-client/src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as PageHeader } from './PageHeader.vue'
+export { default as FormInput } from './FormInput.vue'
+export { default as FormError } from './FormError.vue'
+export { default as SubmitButton } from './SubmitButton.vue'
+export { default as CooldownButton } from './CooldownButton.vue'
+
+export { useCooldown } from '@/composables/useCooldown'
