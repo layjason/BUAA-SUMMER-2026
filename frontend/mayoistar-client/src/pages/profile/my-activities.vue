e624c036d1eb2f56532aeea4ed6791d9694091ef
Lance_zer <Lance_zer@outlook.com>
2026-07-01 01:22:14 +0800

    feat(frontend): 添加个人中心子页面路由和占位页
    8个子页面：编辑资料、我创建的活动、我的报名、我的好友、好友申请、我的小队、黑名单、设置。当前均为占位页面。

---
+++ frontend/mayoistar-client/src/pages/profile/my-activities.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="page">
+    <PageHeader :title="title" />
+    <text class="placeholder">功能开发中</text>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { PageHeader } from '@/components'
+
+const title = '功能开发中'
+</script>
+
+<style scoped>
+.page {
+  min-height: 100vh;
+  background-color: #f7f8fa;
+  padding: 32rpx;
+}
+.placeholder {
+  display: block;
+  text-align: center;
+  font-size: 28rpx;
+  color: #969799;
+  margin-top: 80rpx;
+}
+</style>
