0beec18a72a48ab9f7fa23ceb890ccc072d5e2fb
NonsenseCourse <nonsensecourse@163.com>
2026-07-06 09:14:44 +0800

    style: 格式化代码

--- backend/src/main/java/io/github/layjason/mayoistar/config/SecurityConfiguration.java
+++ backend/src/main/java/io/github/layjason/mayoistar/config/SecurityConfiguration.java
@@ -4,13 +4,13 @@ import io.github.layjason.mayoistar.exception.SecurityErrorHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -49,11 +49,7 @@ public class SecurityConfiguration {
     };
 
     private static final String[] PUBLIC_GET_ENDPOINTS = {
-        "/social/teams",
-        "/social/teams/*",
-        "/social/teams/*/members",
-        "/social/teams/*/points",
-        "/activities/*",
+        "/social/teams", "/social/teams/*", "/social/teams/*/members", "/social/teams/*/points", "/activities/*",
     };
 
     private static final String[] MERCHANT_ENDPOINTS = {
